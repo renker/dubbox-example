8ce2316f8dc92f59af6ed3545e16cec86bdc8e4c
renker <caishenchen@163.com>
2017-02-17 11:10:05 +0800

    调整

--- dubbox-example-client-side/src/main/java/com/renker/example/client/Application.java
+++ dubbox-example-client-side/src/main/java/com/renker/example/client/Application.java
@@ -3,7 +3,6 @@ package com.renker.example.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-
 @SpringBootApplication
 @ImportResource(locations={"classpath:spring-application.xml"})
 public class Application {
